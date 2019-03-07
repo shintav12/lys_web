2962e9fa2f51b19aa8cea8b9bfa5bbb6fa4b595a
Manuel Alzamora <manuel.alzamora@ideamultimedia.net>
2019-03-07 00:38:37 -0500

    fixes

--- resources/views/layout/tags.blade.php
+++ resources/views/layout/tags.blade.php
@@ -1,5 +1,5 @@
 <ul class="tag mtb-50">
     @foreach($tags as $tag)
-        <li><a href="{{url('/tags/'.$tag->slug)}}"><b>{{$tag->name}}</b></a></li>
+        <li><a style="border-radius: 5px;background: lightgray;border: lightgray;color: #6d6d6d;" href="{{url('/tags/'.$tag->slug)}}"><b>{{$tag->name}}</b></a></li>
     @endforeach
 </ul>
