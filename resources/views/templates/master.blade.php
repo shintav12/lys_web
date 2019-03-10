2f1123442071a797500afbb5206e6b275c5c4c19
Manuel Alzamora <manuel.alzamora@ideamultimedia.net>
2019-03-10 16:06:40 -0500

    metas

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
+    <meta name="google-site-verification" content="XegEF40B0N-kaKlQyRimtRALIg1ViM6acrErC94EBRs" />
 	<link href="https://fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet">	
     <link href="{{asset('assets/plugin-frameworks/swiper.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('assets/plugin-frameworks/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
