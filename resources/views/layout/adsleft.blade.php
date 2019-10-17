5d1d399e73f261c9a95c6a7a584dba9e48a6aeb9
Manuel Ivan Alzamora Flores <manuel.alzamora@proximitycr.com>
2019-10-17 00:40:41 -0500

    ads

--- resources/views/layout/adsleft.blade.php
+++ resources/views/layout/adsleft.blade.php
@@ -4,29 +4,12 @@
             <div class="mx-w-md-400x mlr-md-auto">
                 <div class="mb-50">
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- left -->
+                    <!-- ads -->
                     <ins class="adsbygoogle"
                         style="display:block"
                         data-ad-client="ca-pub-3054687449371092"
-                        data-ad-slot="6668221646"
-                        data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
-                </div>                   
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-md-12 col-lg-8">
-            <div class="mx-w-md-400x mlr-md-auto">  
-                <div class="mb-50">
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- left -->
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-client="ca-pub-3054687449371092"
-                        data-ad-slot="6668221646"
+                        data-ad-slot="2752133142"
+                        data-ad-format="auto"
                         data-full-width-responsive="true"></ins>
                     <script>
                         (adsbygoogle = window.adsbygoogle || []).push({});
@@ -40,11 +23,31 @@
             <div class="mx-w-md-400x mlr-md-auto">
                 <div class="mb-50">
                     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                    <!-- left -->
+                    <!-- ads -->
                     <ins class="adsbygoogle"
                         style="display:block"
                         data-ad-client="ca-pub-3054687449371092"
-                        data-ad-slot="6668221646"
+                        data-ad-slot="2752133142"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                </div>                   
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 col-lg-8">
+            <div class="mx-w-md-400x mlr-md-auto">
+                <div class="mb-50">
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- ads -->
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-3054687449371092"
+                        data-ad-slot="2752133142"
+                        data-ad-format="auto"
                         data-full-width-responsive="true"></ins>
                     <script>
                         (adsbygoogle = window.adsbygoogle || []).push({});
