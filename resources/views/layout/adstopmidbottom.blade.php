c4e837672991d3e13abf6a55f5ed1ab708bc7ce8
Manuel Ivan Alzamora Flores <manuel.alzamora@proximitycr.com>
2019-10-17 00:45:25 -0500

    ads

--- resources/views/layout/adstopmidbottom.blade.php
+++ resources/views/layout/adstopmidbottom.blade.php
@@ -1,13 +1,13 @@
 <div class="row">
     <div class="col-xs-12">
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- top/mid/bottom -->
+        <!-- ads -->
         <ins class="adsbygoogle"
             style="display:block"
             data-ad-client="ca-pub-3054687449371092"
-            data-ad-slot="2009617876"
-            data-ad-format="auto"
-            data-full-width-responsive="true"></ins>
+            data-ad-slot="2752133142"
+            data-ad-format="auto">
+        </ins>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({});
         </script>
